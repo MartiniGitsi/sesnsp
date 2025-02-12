e76f5345999769d34de13d385f3035a2196c05c9
MartiniGitsi <100308471+MartiniGitsi@users.noreply.github.com>
2025-02-11 23:09:45 -0600

    Add files via upload

--- sesnsp.py
+++ sesnsp.py
@@ -110,6 +110,12 @@ client.close()
 
 #Va la parte de Streamlit
 
+# App Configuration
+#st.set_page_config(page_title="App de Graficas del SESNSP", layout="wide")
+
+#Título
+st.title("Generación de gráficas delictivas")
+
 #Función para obtener Aniomes estilo np.datetime64
 def get_dt64(Aniomes) -> np.datetime64:
     year = Aniomes // 100
@@ -118,11 +124,18 @@ def get_dt64(Aniomes) -> np.datetime64:
     dt64 = np.datetime64(dt)
     return dt64
 
+# Sidebar
+st.sidebar.header("Ajustes")
+nom_agrupador_selecc = st.sidebar.selectbox("Selecciona un delito:", dfCabAgrp['Nombre_Agrupador_Delito'])
+IdAgrupDel= list (dfCabAgrp.loc[dfCabAgrp['Nombre_Agrupador_Delito'] == nom_agrupador_selecc, 'Id_Agrupador_Delito'])[0]
+
+AnchoBar = st.sidebar.select_slider("Selecciona el ancho de barra:", options= np.arange (5,20,0.5), value =10.5)
+AnchoLinea = st.sidebar.select_slider("Selecciona el ancho de línea:", options= np.arange (2.5,10,0.5), value =4)
+tipo_linea = st.sidebar.selectbox("Selecciona tipo de línea:", ['-', '--', '-.', ':', 'None'])
+
+#Cuerpo
 nom_mun_selecc = st.selectbox("Selecciona un municipio:", dfMun['mun_compuesto'])
 IdMun = list (dfMun.loc[dfMun['mun_compuesto'] == nom_mun_selecc, 'Id_Municipio'])[0]
-
-nom_agrupador_selecc = st.selectbox("Selecciona un delito:", dfCabAgrp['Nombre_Agrupador_Delito'])
-IdAgrupDel= list (dfCabAgrp.loc[dfCabAgrp['Nombre_Agrupador_Delito'] == nom_agrupador_selecc, 'Id_Agrupador_Delito'])[0]
 
 list_aniomes = df['Aniomes'].unique()
 list_aniomes.sort()
@@ -131,8 +144,6 @@ Aniomes_Ini, Aniomes_Fin = st.select_slider(
     options = list_aniomes,
     value = (list_aniomes[-12], list_aniomes[-1]))
 
-AnchoBar = st.select_slider("Selecciona el ancho de barra:", options= np.arange (5,20,0.5), value =10.5)
-AnchoLinea = st.select_slider("Selecciona el ancho de línea:", options= np.arange (2.5,10,0.5), value =4)
 
 #IdMun= 32056
 #Aniomes_Ini= 202206
@@ -158,7 +169,7 @@ fig, ax = plt.subplots(1, 1, figsize=(16.0, 10.0), layout='constrained')
 fig.set_constrained_layout_pads(w_pad=2./12., h_pad=8./12., hspace=0., wspace=0.)
 
 ax.bar ('dt64', 'tasa', data= dftemp, width= AnchoBar,  color='darkslategrey', label='tasa municipal')
-ax.plot ('dt64', 'tasa_nal', data= dftemp, linewidth= AnchoLinea, linestyle= '-.', color='salmon', label='tasa nacional')
+ax.plot ('dt64', 'tasa_nal', data= dftemp, linewidth= AnchoLinea, linestyle= tipo_linea, color='salmon', label='tasa nacional')
 
 ax.bar_label(ax.containers[0], label_type='edge', fontsize= 10, color='black')
 
@@ -181,7 +192,13 @@ fig.legend(loc='lower right', fontsize='10', shadow=True)
 
 st.pyplot(fig)
 
+#Columnas
+col1, col2 = st.columns(2)
+with col1:
+    st.metric(label="Variación del periodo", value="40%", delta="+5%", delta_color= "inverse")
+with col2:
+    st.metric(label="Variación últimos tres meses", value="12%", delta="-2%", delta_color= "inverse")
 
-st.write ("Datos considerados:")
-st.table(dftemp[['Aniomes','tasa','tasa_nal']])
-#st.write (dftemp[['Aniomes','tasa','tasa_nal']])
+
+with st.expander("Ver tabla de datos"):    
+    st.table(dftemp[['Aniomes','tasa','tasa_nal']])
